3f498ca810285179d61dabdb7f39af975029fc17
MaxiVargas78 <maxx7689@gmail.com>
2021-02-18 15:23:32 -0300

    Continuacion API Django

--- BlogInfo/urls.py
+++ BlogInfo/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( '', include('Inicio.urls')),
-]
+    path('summernote/', include('django_summernote.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))] #Enrutador para REACT con expresión regular
 
