7effc29198f8b6b57a23db1185ccad9481774728
MaxiVargas78 <maxx7689@gmail.com>
2021-02-19 15:09:07 -0300

    API Contenido actualizado

--- Contenido/admin.py
+++ Contenido/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import SomeModel
 
 # Register your models here.
+# Apply summernote to all TextField in model.
+class PostBlogAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = ('Contenido',)
+
+admin.site.register(PostBlog, PostBlogAdmin)
+
+
--- Contenido/models.py
+++ Contenido/models.py
@@ -14,12 +14,35 @@ class Categorias(models.TextChoices):
 class PostBlog(models.Model):
     Titulo = models.CharField(max_length= 60)
     Categoria = models.CharField(max_length= 40, choices=Categorias.choices) #se puede incluir un default
-    MiniDes = mdoels.CharField(max_length= 140) #igual que Twitter
+    MiniDes = models.CharField(max_length= 140) #igual que Twitter
     Miniatura = models.ImageField(upload_to='imagenes/%A/%m/%d/') #se organizan por año y mes
     Fecha = models.CharField(max_length= 8)
-    Slug = models.SlugField() 
+    Destacados = models.BooleanField(default=False)
     Contenido = models.TextField()
+    FechaCreacion = models.DateTimeField(default=datetime.now, blank=True)
+    Slug = models.SlugField() 
 
+    #Esta funcion hace que se puedan tener varios titulos iguales y que los slug no coincidan.
+    def Save(self, *args, **kwargs):
+        SlugOriginal= slugify(self.Titulo)
+        queryset = PostBlog.objects.all().filter(slug_iexact=SlugOriginal).count()
+
+        count = 1
+        slug = SlugOriginal
+        while(queryset):
+            slug = SlugOriginal +'-'+ str(count)
+            count +=1
+            queryset = PostBlog.objects.all().filter(slug_iexact=slug).count()
+        self.slug = slug
+
+        if self.Destacados:
+            try:
+                temp = PostBlog.objects.get(Contenido=True)
+                if self != temp:
+                    temp.featured = False
+                    temp.save()
+                except PostBlog.DoesNotExist:
+                    pass
 
 
 
