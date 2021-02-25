59ed11a24d155ef32e3fda636fbfe99c76cf11c8
MaxiVargas78 <maxx7689@gmail.com>
2021-02-25 16:40:18 -0300

    Construcción Inicio front

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,9 +4,13 @@ import {Link, NavLink} from 'react-router-dom'
 
 
 const Home = () => (
-    <nav>
-       Home
-    </nav>
+       <div class="jumbotron">
+            <h1 class="display-4">Bienvenid@s!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4" />
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-primary btn-dark" background='https://www.smartstateindia.com/wp-content/uploads/2020/08/cyber-scurity.jpg' href="#" role="button">Ir al blog</a>
+        </div>
 );
 
 export default Home;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,9 +4,28 @@ import {Link, NavLink} from 'react-router-dom'
 
 
 const NavBar = () => (
-    <nav>
-        NavBar
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand text-white" to='/' href="#">
+            <img src="https://lh3.googleusercontent.com/proxy/atfpYxfPvgJUcfQVjUmNChX0Xqk3CfkdjihEUcsirvfMUBtMBDUYLL5BascTXVKTModqPjBcnmMzt3mmjwaaMrh4HSgIfSqLxH_xbsyjcL_QA7Fdxsxc4-dhTgmIz1D-tQ" width="60" height="40" className="d-inline-block align-top " alt="" loading="lazy"/>
+            Blog Informático
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className='nav-item'>
+                    <NavLink className='nav-link' exact to='/'>Inicio</NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' exact to='/blog'>Blog</NavLink>
+                </li>
+            </ul>
+        </div>
     </nav>
+
+
 );
 
 export default NavBar;
