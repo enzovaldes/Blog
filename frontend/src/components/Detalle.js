b30ebdd4f0489c552fe7db3c51d68aecb4309276
MaxiVargas78 <maxx7689@gmail.com>
2021-03-05 00:47:14 -0300

    fixed error rutas frontend + pagina principal

--- frontend/src/components/Detalle.js
+++ frontend/src/components/Detalle.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import {Link, NavLink} from 'react-router-dom'
+import React, { useState } from 'react';
+import {useEffect} from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 
 
-const Detalle = () => (
-    <nav>
-        Detalle
-    </nav>
-);
+const Detalle = () => {
+    const [blogs, setBlog] = useState({});
+    const CrearBlog = () => {
+        return {__html: blogs.content}
+    };
+
+    return (
+        <div className= 'container mt-3'>
+            <h1 className = 'display-2'>Titulo</h1>
+            <h2 className = 'text-muted mt-3'> Categoria: categoria</h2>
+            <h4>Fecha</h4>
+            <div className = 'mt-5 mb-5' dangerouslySetInnerHTML={CrearBlog()} />
+            <hr />
+            <p className='lead mb-5'><Link to='/blog' className='font-weight-bold'></Link></p>
+
+        </div>
+    )
+    
+};
 
 export default Detalle;
